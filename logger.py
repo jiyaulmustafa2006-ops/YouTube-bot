fc0e040ee0649d5574afc3149650211bc3949119
jiyaulmustafa2006-ops <jiyaulmustafa2006@gmail.com>
2026-04-01 10:38:33 +0530

    Add logger setup in logger.py

---
+++ logger.py
@@ -0,0 +1,14 @@
+import logging
+
+def setup_logger():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler("automation.log"),
+            logging.StreamHandler()
+        ]
+    )
+    return logging.getLogger(__name__)
+
+logger = setup_logger()
